3aca3e7e5872b7bb1d402ae769d63769b7023f2f
andre <andrearante12@gmail.com>
2024-01-17 00:37:14 -0500

    added missing files

---
+++ src/turtle/MainPhase.java
@@ -0,0 +1,92 @@
+package turtle;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import java.util.Random;
+
+
+import battlecode.common.*;
+
+public class MainPhase {
+    
+  public static void runMainPhase(RobotController rc) throws GameActionException {
+
+    // Buy global upgrade 
+    if(rc.canBuyGlobal(GlobalUpgrade.HEALING)) {
+      rc.buyGlobal(GlobalUpgrade.HEALING);
+    } 
+    else if(rc.canBuyGlobal(GlobalUpgrade.ACTION)) {
+      rc.buyGlobal(GlobalUpgrade.ACTION);
+    }
+
+    //attack enemies, prioritizing enemies that have your flag
+    RobotInfo[] nearbyEnemies = rc.senseNearbyRobots(-1, rc.getTeam().opponent());
+    for(RobotInfo robot : nearbyEnemies) {
+      if(robot.hasFlag()) {
+        Pathfind.moveTowards(rc, robot.getLocation(), true);
+        if(rc.canAttack(robot.getLocation())) rc.attack(robot.getLocation());
+      }
+      if(rc.canAttack(robot.getLocation())) {
+        rc.attack(robot.getLocation());
+      }
+    }
+    
+    // 50/50 Chance of Building either a Water or explosive trap
+    Random rand = new Random();
+    int chance = rand.nextInt(10);
+    // if(rc.canBuild(TrapType.WATER, rc.getLocation()) && chance > 7) {
+    //   rc.build(TrapType.WATER, rc.getLocation());
+    // } 
+    
+    if (rc.canBuild(TrapType.EXPLOSIVE, rc.getLocation())){
+      rc.build(TrapType.EXPLOSIVE, rc.getLocation());
+    }
+
+    // If we can't build either just dig
+    // else {
+    //   MapLocation waterLoc = rc.getLocation().add(RobotPlayer.directions[RobotPlayer.random.nextInt(8)]);
+    //   if(rc.canDig(waterLoc)) rc.dig(waterLoc);
+    // }
+
+    //move towards flags and place defenses around them
+    MapLocation turtle = new MapLocation(rc.readSharedArray(0), rc.readSharedArray(1));
+
+    // Place traps and water around flag
+    Pathfind.moveTowards(rc, turtle, false);
+    if(rc.getLocation().distanceSquaredTo(turtle) > 9 && rc.getLocation().distanceSquaredTo(turtle) < 64) {
+      MapLocation waterLoc = rc.getLocation().add(RobotPlayer.directions[RobotPlayer.random.nextInt(8)]);
+      if(rc.canDig(waterLoc)) rc.dig(waterLoc);
+    }
+    
+    //try to heal friendly robots
+    for(RobotInfo robot : rc.senseNearbyRobots(-1, rc.getTeam())) {
+      if(rc.canHeal(robot.getLocation())) rc.heal(robot.getLocation());
+    }
+
+    // If we don't have the flag and aren't already near the turtle, try to move towards the turtle 
+    if(!rc.hasFlag() && rc.getLocation().distanceSquaredTo(turtle) > 9) {
+      Pathfind.moveTowards(rc, turtle, false);
+    }
+    else {
+      //if we have the flag, move towards the closest ally spawn zone
+      MapLocation[] spawnLocs = rc.getAllySpawnLocations();
+      MapLocation closestSpawn = findClosestLocation(rc.getLocation(), Arrays.asList(spawnLocs));
+      Pathfind.moveTowards(rc, closestSpawn, true);
+    }
+  }
+
+    public static MapLocation findClosestLocation(MapLocation me, List<MapLocation> otherLocs) {
+        MapLocation closest = null;
+        int minDist = Integer.MAX_VALUE;
+        for(MapLocation loc : otherLocs) {
+            int dist = me.distanceSquaredTo(loc);
+            if(dist < minDist) {
+                minDist = dist;
+                closest = loc;
+            }
+        }
+        return closest;
+    }
+}
---
+++ src/turtle/Setup.java
@@ -0,0 +1,142 @@
+package turtle;
+
+import battlecode.common.*;
+
+public class Setup {
+
+    private static final int EXPLORE_ROUNDS = 150;
+    private static boolean t = false;
+
+    public static void runSetup(RobotController rc) throws GameActionException {
+        if (!t) {
+          rc.writeSharedArray(3, 0);
+          rc.writeSharedArray(4, 0);
+          rc.writeSharedArray(5, 0);
+          t = !t;
+        }  
+
+        if(rc.getRoundNum() < EXPLORE_ROUNDS) {
+
+            /*
+            MapLocation center = new MapLocation(rc.getMapWidth()/2,rc.getMapHeight()/2);
+            MapLocation currentLoc = new MapLocation(rc.getLocation().x, rc.getLocation().y);
+            int distFromCenter = center.distanceSquaredTo(currentLoc);
+
+            if(distFromCenter > rc.readSharedArray(1000)) {
+                rc.writeSharedArray(1000, distFromCenter);
+                rc.writeSharedArray(2000, currentLoc.x);
+                rc.writeSharedArray(3000, currentLoc.y);
+            }
+            */
+
+            FlagInfo[] flags = rc.senseNearbyFlags(-1);
+            for(FlagInfo flag : flags) {
+                MapLocation flagLoc = flag.getLocation();
+                if(rc.senseMapInfo(flagLoc).isSpawnZone() && rc.canPickupFlag(flagLoc)) {
+                    rc.pickupFlag(flag.getLocation());
+                }
+            }
+            
+            // If we don't have a flag, explore randomly (search for bread crumbs)
+            if (!rc.hasFlag()){
+                Pathfind.explore(rc);
+
+                MapLocation center = new MapLocation(rc.getMapWidth()/2,rc.getMapHeight()/2);
+                MapLocation currentLoc = new MapLocation(rc.getLocation().x, rc.getLocation().y);
+                int distFromCenter = center.distanceSquaredTo(currentLoc);
+
+                if(distFromCenter > rc.readSharedArray(3)) {
+                    rc.writeSharedArray(3, distFromCenter);
+                    rc.writeSharedArray(4, currentLoc.x);
+                    rc.writeSharedArray(5, currentLoc.y);
+                    rc.setIndicatorString("holding friendly flag");
+                }
+            }
+
+            // If we do have a flag, try to bring it to a corner
+            else {
+              Pathfind.findCorner(rc);
+              rc.setIndicatorString("holding friendly flag");
+            }
+
+
+        }
+        // During turns 175 - 200, try and place a flag (that has hopefully been brought to a corner)
+        else if (rc.getRoundNum() < 200) {
+
+          //try to place flag if it is far enough away from other flags
+          if(rc.senseLegalStartingFlagPlacement(rc.getLocation())) {
+            if(rc.canDropFlag(rc.getLocation())) {
+              rc.dropFlag(rc.getLocation());
+              rc.writeSharedArray(0, rc.getLocation().x);
+              rc.writeSharedArray(1, rc.getLocation().y);
+            }
+          } else {
+            // If we can't find a spot to place flag, move away from the nearest flag
+            Pathfind.placeFlag(rc);
+          }
+
+          //move towards flags and place defenses around them
+          FlagInfo[] flags = rc.senseNearbyFlags(-1);
+          FlagInfo targetFlag = null;
+          for(FlagInfo flag : flags) {
+            if(!flag.isPickedUp()) {
+              targetFlag = flag;
+              break;
+            }
+          }
+
+          // If there is a nearby flag
+          if(targetFlag != null) {
+            // Move towards it
+            Pathfind.moveTowards(rc, targetFlag.getLocation(), false);
+
+            // Try to place an explosive trap
+            if(rc.getLocation().distanceSquaredTo(flags[0].getLocation()) < 9) {
+              if(rc.canBuild(TrapType.EXPLOSIVE, rc.getLocation())) {
+                rc.build(TrapType.EXPLOSIVE, rc.getLocation());
+              }
+              else {
+                MapLocation waterLoc = rc.getLocation().add(RobotPlayer.directions[RobotPlayer.random.nextInt(8)]);
+                if(rc.canDig(waterLoc)) rc.dig(waterLoc);
+              }
+            }
+          } 
+          else {
+            Pathfind.explore(rc);
+          } 
+
+        }
+        else {
+            //try to place flag if it is far enough away from other flags
+            if(rc.senseLegalStartingFlagPlacement(rc.getLocation())) {
+                if(rc.canDropFlag(rc.getLocation())) rc.dropFlag(rc.getLocation());
+            }
+
+            //move towards flags and place defenses around them
+            FlagInfo[] flags = rc.senseNearbyFlags(-1);
+
+            FlagInfo targetFlag = null;
+            for(FlagInfo flag : flags) {
+                if(!flag.isPickedUp()) {
+                    targetFlag = flag;
+                    break;
+                }
+            }
+
+            if(targetFlag != null) {
+                Pathfind.moveTowards(rc, targetFlag.getLocation(), false);
+                if(rc.getLocation().distanceSquaredTo(flags[0].getLocation()) < 9) {
+                    if(rc.canBuild(TrapType.EXPLOSIVE, rc.getLocation())) {
+                        rc.build(TrapType.EXPLOSIVE, rc.getLocation());
+                    }
+                    else {
+                        MapLocation waterLoc = rc.getLocation().add(RobotPlayer.directions[RobotPlayer.random.nextInt(8)]);
+                        if(rc.canDig(waterLoc)) rc.dig(waterLoc);
+                    }
+                }
+            } 
+            else Pathfind.explore(rc);
+        }
+    }
+}
